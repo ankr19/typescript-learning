86139a00f4072814579c746c76fa8363b133ad96
anand kumar <ankm335@gmail.com>
2024-05-10 11:03:58 +0530

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
